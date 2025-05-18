ebaa3d067675d5d391ca24edfcbcc0ed7d687131
VivaCristioRey <chris.jara0626@gmail.com>
2025-05-18 10:41:59 -0500

    .

---
+++ OperacionesEnteros/enteros.js
@@ -0,0 +1,13 @@
+sumar = (a, b) => {
+return a + b
+}
+
+restar = (a, b) => {
+    return a - b
+}
+
+multiplicar = (a, b) => {
+    return a * b
+}
+
+export const calc = {multiplicar, restar, sumar}
---
+++ index.js
@@ -0,0 +1,18 @@
+// import {sumar, resta} from ('./OperacionesEnteros/enteros')
+const express = require('express')
+const app = express()
+const port = 8888
+
+app.get('/quieroSumar', (req, res) => {
+    res.send('<h1>Hola, ¿Qué quieres sumar?</h1>')
+})
+
+app.get()
+
+app.get('', (req, res) => {
+    res.send('<h1>Hola, ¡Bienvenido!</h1>')
+})
+
+app.listen(port, () => {
+    console.log('App en línea')
+})
